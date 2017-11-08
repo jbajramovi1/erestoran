0e010b80aeddd414bddb6ba0e2918ecbd69a8624
Jasmina Bajramovic <jasmina@Jasminas-MacBook-Pro.local>
2017-11-08 15:52:34 +0100

    RES-1:Create admin panel and dashboard component

--- public/eRestoran_FE/app/router.js
+++ public/eRestoran_FE/app/router.js
@@ -16,6 +16,7 @@ Router.map(function() {
   this.route('login');
   this.route('register');
   this.route('restaurant',{path: 'restaurant/:id'});
+  this.route('adminpanel');
 });
 
 export default Router;
