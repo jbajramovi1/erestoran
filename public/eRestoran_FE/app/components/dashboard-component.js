e5b44d69c9e3459c4a6c870d293894e3e831896f
Jasmina Bajramovic <jasmina@Jasminas-MacBook-Pro.local>
2017-11-11 18:46:55 +0100

    Add restaurant component

--- public/eRestoran_FE/app/components/dashboard-component.js
+++ public/eRestoran_FE/app/components/dashboard-component.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  adminpanel:Ember.inject.controller(),
+  actions:{
+    openAddRestaurant:function(){
+        this.get('adminpanel').send('openAddRestaurant');
+    },
+    openAddLocation:function(){
+        this.get('adminpanel').send('openAddLocation');
+    }
+  }
 });
