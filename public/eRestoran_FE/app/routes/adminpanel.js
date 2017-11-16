5101ded0a56041ad5ecb78bf26d29a50e3c29d2f
Jasmina Bajramovic <jasmina@Jasminas-MacBook-Pro.local>
2017-11-16 15:33:10 +0100

    final adminapanel fix

--- public/eRestoran_FE/app/routes/adminpanel.js
+++ public/eRestoran_FE/app/routes/adminpanel.js
@@ -3,16 +3,22 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   sessionService:Ember.inject.service('session-service'),
   notifications: Ember.inject.service('notification-messages'),
+  accountService:Ember.inject.service('account-service'),
   setupController: function(controller, model) {
     this._super(controller, model);
   },
   beforeModel(transition) {
-    if (!this.get('sessionService').isAdmin()) {
-      this.get('notifications').error("You can't access admin panel without permissions!", {
-       autoClear: true,
-       clearDuration: 1500
-        });
-      this.transitionTo('home');
-    }
+      this.get('accountService').getSession()
+      .done(response => {
+             if (response.role!=="ADMIN"){
+               this.get('notifications').error("You can't access admin panel without permissions!", {
+                autoClear: true,
+                clearDuration: 1500
+                 });
+               this.transitionTo('home');
+           }
+          });
+
+
   }
 });
